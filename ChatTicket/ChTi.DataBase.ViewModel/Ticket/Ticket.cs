0abc1deaeec569490940f5d9e212ebf97f97b5ba
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-08 12:33:57 -0700

    add new items for ticket view models

--- ChatTicket/ChTi.DataBase.ViewModel/Ticket/Ticket.cs
+++ ChatTicket/ChTi.DataBase.ViewModel/Ticket/Ticket.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace ChTi.DataBase.ViewModel;
 
-public record TicketViewModel(Guid Id, Guid FromId, Guid ToId, string Subject, string Description, IEnumerable<UploadFileViewModel> Files);
+public record TicketViewModel(Guid Id, Guid FromId, Guid ToId, string Subject, string Description,string CreateDate, TicketStatusViewModel Status, IEnumerable<UploadFileViewModel> Files);
 
 public record UpsertTicket(Guid? Id, Guid ToUser, string Subject, string Description);
 
 public record AddAttachments(Guid TicketId,IEnumerable<UploadFileViewModel> Files);
 
 public record UpsertTicketResponse(UpsertTicketStatus Status, TicketViewModel? Ticket);
+
+public record TicketStatusViewModel(short Code,string Name);
 
 public enum UpsertTicketStatus
 {
