64b79acff244b6186936640c896dd8b2696e7bd2
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-08 12:33:18 -0700

    ticket view model services

---
+++ ChatTicket/ChTi.Service.Abstraction/Ticket/ITicketViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ChTi.DataBase.Entity;
+using ChTi.DataBase.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Service.Abstraction;
+
+public interface ITicketViewModel : IAsyncDisposable
+{
+    Task<TicketViewModel> CreateTicketViewModelAsync(Ticket ticket);
+
+    Task<IEnumerable<TicketViewModel>> CreateTicketViewModelAsync(IEnumerable<Ticket> tickets);
+}
---
+++ ChatTicket/ChTi.Service.Implemention/Ticket/TicketViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ChTi.Service.Tools.Date;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Service.Implemention;
+
+public class TicketViewModel : ITicketViewModel
+{
+    public Task<DataBase.ViewModel.TicketViewModel> CreateTicketViewModelAsync(Ticket ticket)
+    {
+        DataBase.ViewModel.TicketViewModel viewModel = new(
+            Id: ticket.Id,
+            FromId: ticket.FromUserId,
+            ToId: ticket.ToUserId, Subject: ticket.Subject,
+            Description: ticket.Description,
+            CreateDate: ticket.CreateDate.ToShamsi(),
+            Status: new(ticket.Status, (TicketStatus)ticket.Status switch
+            {
+                TicketStatus.Open => "باز",
+                TicketStatus.Close => "بسته شده",
+                _ => "باز"
+            }),
+            Files: default);
+        return Task.FromResult(viewModel);
+    }
+
+    public async Task<IEnumerable<DataBase.ViewModel.TicketViewModel>> CreateTicketViewModelAsync(IEnumerable<Ticket> tickets)
+    {
+        List<DataBase.ViewModel.TicketViewModel> result = new();
+        foreach (var item in tickets)
+            result.Add(await CreateTicketViewModelAsync(item));
+        return result;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
