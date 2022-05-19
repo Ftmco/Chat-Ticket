179c45f58c95b3cce79cfd09aaa2d24fa25c996b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:41:38 -0700

    create ticket services

---
+++ ChatTicket/ChTi.Client/Abstraction/ITicket.cs
@@ -0,0 +1,15 @@
+﻿using ChTi.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Client.Abstraction;
+
+public interface ITicket : IAsyncDisposable
+{
+    Task<Ticket?> UpsertAsync(UpsertTicket upsert);
+
+    Task<IEnumerable<Attechment>> AddAttechmentsAsync(IEnumerable<AddAttechment> attechments);
+}
---
+++ ChatTicket/ChTi.Client/Implemention/Ticket.cs
@@ -0,0 +1,61 @@
+﻿using ChTi.Client.Abstraction;
+using ChTi.Client.Model;
+using ChTi.Web.Grpc;
+using TicketClient = ChTi.Web.Grpc.Ticket.TicketClient;
+
+namespace ChTi.Client.Implemention;
+
+public class Ticket : ITicket
+{
+    readonly IGrpcRule _gRPC;
+
+    public Ticket(IGrpcRule gRPC)
+    {
+        _gRPC = gRPC;
+    }
+
+    public async Task<IEnumerable<Attechment>> AddAttechmentsAsync(IEnumerable<AddAttechment> attechments)
+    {
+        var channel = await _gRPC.OpenChannelAsync();
+        TicketClient client = new(channel);
+        var request = client.AddAttechments();
+        foreach (var attechment in attechments)
+            await request.RequestStream.WriteAsync(new()
+            {
+                Base64 = attechment.Base64,
+                Name = attechment.Name,
+            });
+        await request.RequestStream.CompleteAsync();
+        AddAttechmentReply? response = await request.ResponseAsync;
+        return response.Attechments.Select((attechment) =>
+        {
+            _ = Guid.TryParse(attechment.Id, out Guid id);
+            return new Attechment(Id: id, FileToken: attechment.FileToken, Name: attechment.Name);
+        });
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        await _gRPC.DisposeAsync();
+    }
+
+    public async Task<Model.Ticket?> UpsertAsync(UpsertTicket upsert)
+    {
+        var channel = await _gRPC.OpenChannelAsync();
+        TicketClient client = new(channel);
+        TicketReply? request = await client.UpsertTicketAsync(new()
+        {
+            FromUser = upsert.FromUser.ToString(),
+            Subject = upsert.Subject,
+            Text = upsert.Text,
+            ToUser = upsert.ToUser.ToString()
+        });
+        if (request != null)
+        {
+            _ = Guid.TryParse(request.TicketId, out Guid ticketId);
+            return new Model.Ticket(Id: ticketId, Subject: request.Subject, Text: request.Text);
+        }
+        return null;
+    }
+}
