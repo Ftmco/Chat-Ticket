7b9d0a9835898c064d3c88b3d83b60679eaa5f8f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:42:30 -0700

    create start up for control dependencies

---
+++ ChatTicket/ChTi.Client/StartUp/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using ChTi.Client.Abstraction;
+using ChTi.Client.Cache;
+using ChTi.Client.Implemention;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChTi.Client.StartUp;
+
+public static class StartUp
+{
+    public static IServiceCollection AddFteamChTiService(this IServiceCollection service)
+    {
+        service.AddTransient<ICache, Cache.Cache>();
+        service.AddTransient<IGrpcRule, GrpcService>();
+
+        return service;
+    }
+}
