d33b8b3c55265e5a09f86e8e0f7a398bf8143aa7
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:41:06 -0700

    create ticket proto

---
+++ ChatTicket/ChTi.Web.Grpc/Services/TicketService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace ChTi.Web.Grpc.Services;
+
+public class TicketService : Ticket.TicketBase
+{
+    public override Task<TicketReply> UpsertTicket(UpsertRequest request, ServerCallContext context)
+    {
+        return base.UpsertTicket(request, context);
+    }
+
+    public override Task<AddAttechmentReply> AddAttechments(IAsyncStreamReader<AddAttechmentRequest> requestStream, ServerCallContext context)
+    {
+        return base.AddAttechments(requestStream, context);
+    }
+}
