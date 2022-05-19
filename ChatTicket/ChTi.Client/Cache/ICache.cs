95c9cb0a7962089159ca76638fd575d7ca5e33a8
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:40:38 -0700

    create cache service

---
+++ ChatTicket/ChTi.Client/Cache/ICache.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+namespace ChTi.Client.Cache;
+
+public interface ICache : IAsyncDisposable
+{
+    Task<TModel?> GetItemAsync<TModel>(string key);
+
+    Task<TModel?> SetItemAsync<TModel>(string key, TModel obj);
+
+    Task<ConnectionMultiplexer> ConnectAsync();
+}
