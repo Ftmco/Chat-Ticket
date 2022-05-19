c937589a095d2685c13f65ed71cce48bcb646d58
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:41:57 -0700

    Create Ticket.cs

---
+++ ChatTicket/ChTi.Client/Model/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Client.Model;
+
+public record UpsertTicket(Guid FromUser, Guid ToUser, string Subject, string Text);
+
+public record Ticket(Guid Id,string Subject,string Text);
+
+public record AddAttechment(string Base64,string Name);
+
+public record Attechment(Guid Id,string FileToken,string Name);
