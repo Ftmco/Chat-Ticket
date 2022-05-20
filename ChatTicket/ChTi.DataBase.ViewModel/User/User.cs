6aaaa498caa8ac7d5b2a8b84d8d35440ff72f27c
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-20 12:49:43 -0700

    create user view model

---
+++ ChatTicket/ChTi.DataBase.ViewModel/User/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.DataBase.ViewModel;
+
+public record UserViewModel(Guid Id, string FullName, string Email, string MobileNo, IEnumerable<FileViewModel>? Avatar);
