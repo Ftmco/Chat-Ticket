a147ebc98088f4c7e438ffe8538784346b3620fc
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-20 12:49:03 -0700

    create chat entities

---
+++ ChatTicket/ChTi.DataBase.Entity/Chat/Chat.cs
@@ -0,0 +1,30 @@
+﻿namespace ChTi.DataBase.Entity;
+
+public record Chat
+{
+    [BsonId]
+    public Guid Id { get; set; }
+
+    [BsonRequired]
+    [BsonElement("token")]
+    public string Token { get; set; }
+
+    [BsonRequired]
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("description")]
+    public string Description { get; set; }
+
+    [BsonRequired]
+    [BsonElement("createDate")]
+    public DateTime CreateDate { get; set; }
+
+    [BsonRequired]
+    [BsonElement("status")]
+    public short Status { get; set; }
+
+    [BsonRequired]
+    [BsonElement("type")]
+    public short Type { get; set; }
+}
---
+++ ChatTicket/ChTi.DataBase.Entity/Message/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace ChTi.DataBase.Entity;
+
+public record Message
+{
+    [BsonId]
+    public Guid Id { get; set; }
+
+    [BsonRequired, BsonElement("userId")]
+    public Guid UserId { get; set; }
+
+    [BsonRequired, BsonElement("chatId")]
+    public Guid ChatId { get; set; }
+
+    [BsonElement("text")]
+    public string Text { get; set; }
+
+    [BsonRequired, BsonElement("createDate")]
+    public DateTime CreateDate { get; set; }
+}
