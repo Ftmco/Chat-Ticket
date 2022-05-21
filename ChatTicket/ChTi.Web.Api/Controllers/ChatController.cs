e2e4f563cc6171891f298d20fc900ee9e6d9de10
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-21 02:26:51 -0700

    create chat & message controllers

---
+++ ChatTicket/ChTi.Web.Api/Controllers/ChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChTi.Web.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatController : ControllerBase
+{
+    [HttpGet("ChatDetail")]
+    public async Task<IActionResult> GetChatDetailAsync(Guid chatId)
+    {
+        return Ok();
+    }
+}
---
+++ ChatTicket/ChTi.Web.Api/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChTi.Web.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    [HttpGet("GetMessages")]
+    public async Task<IActionResult> GetMessagesAsync(Guid chatId, long lastMessageId)
+    {
+        return Ok();
+    }
+
+    [HttpPost("SendMessage")]
+    public async Task<IActionResult> SendMessageAsync()
+    {
+        return Ok();
+    }
+
+    [HttpPut("UpdateMessage")]
+    public async Task<IActionResult> UpdateMessageAsync()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("DeleteMessage")]
+    public async Task<IActionResult> DeleteMessageAsync()
+    {
+        return Ok();
+    }
+}
