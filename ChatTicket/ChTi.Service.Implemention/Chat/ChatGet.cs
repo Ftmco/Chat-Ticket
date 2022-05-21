64557d3ff785dbc701dfb945c7515c4d15298d62
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-21 02:52:20 -0700

    add chat services

--- ChatTicket/ChTi.Service.Implemention/Chat/ChatGet.cs
+++ ChatTicket/ChTi.Service.Implemention/Chat/ChatGet.cs
@@ -2,8 +2,37 @@
 
 public class ChatGet : IChatGet
 {
+    readonly IChatViewModel _chatViewModel;
+
+    readonly IBaseQuery<Chat> _chatQuery;
+
+    public ChatGet(IChatViewModel chatViewModel, IBaseQuery<Chat> chatQuery)
+    {
+        _chatViewModel = chatViewModel;
+        _chatQuery = chatQuery;
+    }
+
     public ValueTask DisposeAsync()
     {
-        throw new NotImplementedException();
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<Chat?> GetChatAsync(string chatToken)
+        => await _chatQuery.GetAsync(c => c.Token == chatToken);
+
+    public async Task<Chat?> GetChatAsync(Guid chatId)
+        => await _chatQuery.GetAsync(chatId);
+
+    public async Task<ChatDetailViewModel?> GetChatDetailAsync(string chatToken)
+    {
+        Chat? chat = await GetChatAsync(chatToken);
+        return await _chatViewModel.CreateChatDetailViewModeAsync(chat);
+    }
+
+    public async Task<ChatDetailViewModel?> GetChatDetailAsync(Guid chatId)
+    {
+        Chat? chat = await GetChatAsync(chatId);
+        return await _chatViewModel.CreateChatDetailViewModeAsync(chat);
     }
 }
