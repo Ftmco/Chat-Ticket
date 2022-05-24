f4124dd8b530a079a7ea4d6956098d86a3a62709
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:19:01 -0700

    Update GlobalUsings.cs

--- ChatTicket/ChTi.Service.Implemention/GlobalUsings.cs
+++ ChatTicket/ChTi.Service.Implemention/GlobalUsings.cs
@@ -2,3 +2,6 @@
 global using ChTi.DataBase.ViewModel;
 global using ChTi.Service.Abstraction;
 global using ChTi.Service.Abstraction.Base;
+global using MongoDB.Driver;
+global using Identity.Client.Rules;
+global using Identity.Client.Models;
