1222e49fbd658146ebf0edc6b49d8cdc96eb725b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:18:43 -0700

    create code generator tool

---
+++ ChatTicket/ChTi.Service.Tools/Code/Code.cs
@@ -0,0 +1,23 @@
+﻿using ChTi.Service.Tools.Crypto;
+
+namespace Identity.Service.Tools.Code;
+
+public static class CodeExtension
+{
+    public static string CreateCode(this int length)
+    {
+        string? token = length.CreateToken();
+        return token.GetHashCode().ToString();
+    }
+
+    public static string CreateCode(this short length)
+        => ((int)length).CreateCode();
+
+    public static string CreateToken(this int length)
+    {
+        string newString = Guid.NewGuid().ToString().Replace("-", "");
+        while (newString.Length < length)
+            newString += Guid.NewGuid().ToString().Replace("-", "");
+        return newString[0..length].CreateSHA256();
+    }
+}
