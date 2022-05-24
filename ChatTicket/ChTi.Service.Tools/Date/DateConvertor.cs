43a592db34c496a9c4b771f4dcec166316dfac86
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:19:57 -0700

    create time only tool

--- ChatTicket/ChTi.Service.Tools/Date/DateConvertor.cs
+++ ChatTicket/ChTi.Service.Tools/Date/DateConvertor.cs
@@ -9,4 +9,10 @@ public static class DateConvertor
         PersianCalendar pc = new();
         return $"{pc.GetYear(date)}/{pc.GetMonth(date)}/{pc.GetDayOfMonth(date)}";
     }
+
+    public static string ToShamsiTime(this DateTime date)
+    {
+        PersianCalendar pc = new();
+        return $"{pc.GetHour(date)}:{pc.GetMinute(date)}";
+    }
 }
