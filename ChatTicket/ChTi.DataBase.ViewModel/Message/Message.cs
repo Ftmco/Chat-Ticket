48846d2dd5e504fcd70d3b467d8c7dc11b3ef329
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:16:13 -0700

    message view models

---
+++ ChatTicket/ChTi.DataBase.ViewModel/Message/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace ChTi.DataBase.ViewModel;
+
+public record SendMessageViewModel(string ChatToken, string Text, long? ReplyMessageId, Guid? FileId, string? FileToken);
+
+public record MessageViewModel(Guid Id, long MessageId, string? ChatToken,
+    string Text, string SendDate, string SendTime,
+    long? ReplyMessageId, long ReplyCount, UserViewModel? User);
+
+public record SendMessageResponse(MessageActionStatus Status, MessageViewModel? Message);
+
+
+public enum MessageActionStatus
+{
+    Success = 0,
+    AccessDenied = 1,
+    Exception = 2,
+    MessageNotFound = 3,
+    ChatNotFound = 4
+}
