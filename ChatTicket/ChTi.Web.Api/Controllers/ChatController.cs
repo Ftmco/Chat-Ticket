101f85e2733e4c79aa086cffae6ba0b70dca68d0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:19:46 -0700

    Update ChatController.cs

--- ChatTicket/ChTi.Web.Api/Controllers/ChatController.cs
+++ ChatTicket/ChTi.Web.Api/Controllers/ChatController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChTi.DataBase.Entity;
+using ChTi.Service.Abstraction.Base;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChTi.Web.Api.Controllers;
 
@@ -6,9 +8,42 @@ namespace ChTi.Web.Api.Controllers;
 [ApiController]
 public class ChatController : ControllerBase
 {
-    [HttpGet("ChatDetail")]
-    public async Task<IActionResult> GetChatDetailAsync(Guid chatId)
+    readonly IChatGet _chatGet;
+
+    readonly IChatAction _chatAction;
+
+    public ChatController(IChatGet chatGet,IChatAction chatAction)
     {
-        return Ok();
+        _chatGet = chatGet;
+        _chatAction = chatAction;
+    }
+
+    [HttpGet("ChatDetail")]
+    public async Task<IActionResult> GetChatDetailAsync(string chatToken)
+    {
+        ChatDetailViewModel? chat = await _chatGet.GetChatDetailAsync(chatToken);
+        return Ok(chat != null? Success($"جزئیات گفنگو {chat.Name}", "", chat) : Faild(404,"گفنگو یافت نشد",""));
+    }
+
+    [HttpPost("Upsert")]
+    public async Task<IActionResult> UpsertAsync(UpsertChatViewModel upsert)
+    {
+        UpsertChatResponse chat = await _chatAction.UpsertChatAsync(upsert, Request.Headers);
+
+        return chat.Status switch
+        {
+            ChatActionStatus.Success => Ok(Success("گفتگو با موفقیت ثبت شد","",chat.Chat)),
+            ChatActionStatus.UserNotAuthorized => Ok(Faild(403,"برای ایجاد گفتگو وارد حساب خود شوید","")),
+            ChatActionStatus.Exception => Ok(ApiException()),
+            ChatActionStatus.ChatNotFound => Ok(Faild(404,"گفتگو یافت نشد","")),
+            _ => Ok(ApiException()),
+        };
+    }
+
+    [HttpGet("Chats")]
+    public async Task<IActionResult> GetChatsAsync()
+    {
+        IEnumerable<ChatDetailViewModel> chats = await _chatGet.GetUserChatsAsync(Request.Headers);
+        return Ok(Success("گفتگو های کاربر","",chats));
     }
 }
