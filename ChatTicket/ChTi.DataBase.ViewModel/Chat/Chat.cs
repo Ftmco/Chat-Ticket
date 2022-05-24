8feb7be28e583b97dcdce4f3ed8e3f2843d404a7
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:16:05 -0700

    Update Chat.cs

--- ChatTicket/ChTi.DataBase.ViewModel/Chat/Chat.cs
+++ ChatTicket/ChTi.DataBase.ViewModel/Chat/Chat.cs
@@ -1,7 +1,16 @@
 ﻿namespace ChTi.DataBase.ViewModel;
 
-public record ChatDetailViewModel(Guid Id, string Token, string Name, string Bio,
-    string CreateDate, long LastMessageId, ChatTypeViewModel Type, IEnumerable<FileViewModel>? Avatars);
+public record ChatDetailViewModel(Guid Id, string Token, string Name, string Description,
+    string CreateDate, string UpdateDate, long LastMessageId,
+        ChatTypeViewModel Type, IEnumerable<FileViewModel>? Avatars);
+
+public record UpsertChatViewModel(Guid? Id, string Name, string Description, ChatType Type);
+
+public record UpsertChatResponse(ChatActionStatus Status, ChatDetailViewModel? Chat);
+
+public record ChatTypeViewModel(string Type, short Code);
+
+public record AddUserToChatViewModel(Guid? ChatId,Guid UserId, ChatUserType UserType);
 
 public enum ChatType
 {
@@ -9,4 +18,25 @@ public enum ChatType
     Pv = 1
 }
 
-public record ChatTypeViewModel(string Type, short Code);
+public enum ChatStatus
+{
+    Active = 0,
+    Deleted = 1
+}
+
+public enum ChatActionStatus
+{
+    Success = 0,
+    UserNotAuthorized = 1,
+    Exception = 2,
+    ChatNotFound = 3,
+    AccessDenied = 4,
+}
+
+public enum ChatUserType
+{
+    Owner = 0,
+    Admin = 1,
+    User = 2
+}
+
