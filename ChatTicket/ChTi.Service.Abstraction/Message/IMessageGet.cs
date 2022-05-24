16155faf2f751ab2d070d05094b1f92bce73f0ae
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-24 07:17:51 -0700

    get message services

--- ChatTicket/ChTi.Service.Abstraction/Message/IMessageGet.cs
+++ ChatTicket/ChTi.Service.Abstraction/Message/IMessageGet.cs
@@ -2,4 +2,9 @@
 
 public interface IMessageGet : IAsyncDisposable
 {
+    Task<long> GetLastMessageIdAsync(Guid chatId);
+
+    Task<IEnumerable<MessageViewModel>> GetMessagesAsync(Guid chatId, long lastMessageId);
+
+    Task<IEnumerable<MessageViewModel>> GetMessagesAsync(string chatToken, long lastMessageId);
 }
--- ChatTicket/ChTi.Service.Implemention/Message/MessageGet.cs
+++ ChatTicket/ChTi.Service.Implemention/Message/MessageGet.cs
@@ -2,8 +2,42 @@
 
 public class MessageGet : IMessageGet
 {
+    readonly IBaseQuery<Message> _messageQuery;
+
+    readonly IBaseQuery<Chat> _chatQuery;
+
+    readonly IMessageViewModel _messageViewModel;
+
+    public MessageGet(IBaseQuery<Message> messageQuery,IMessageViewModel messageViewModel, IBaseQuery<Chat> chatQuery)
+    {
+        _messageQuery = messageQuery;
+        _messageViewModel = messageViewModel;
+        _chatQuery = chatQuery; 
+    }
+
     public ValueTask DisposeAsync()
     {
-        throw new NotImplementedException();
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<long> GetLastMessageIdAsync(Guid chatId)
+    {
+        Message? message = await _messageQuery.MaxAsync(m => m.ChatId == chatId, m => m.MessageId);
+        return message == null ? 0 : message.MessageId;
+    }
+
+    public async Task<IEnumerable<MessageViewModel>> GetMessagesAsync(Guid chatId, long lastMessageId)
+    {
+        IEnumerable<Message>? messages = await _messageQuery.GetAllAsync(m => m.ChatId == chatId && m.MessageId > lastMessageId);
+        return await _messageViewModel.CreateMessageViewModelAsync(messages);
+    }
+
+    public async Task<IEnumerable<MessageViewModel>> GetMessagesAsync(string chatToken, long lastMessageId)
+    {
+        var chat = await _chatQuery.GetAsync(c => c.Token == chatToken);
+        if(chat == null)
+            return new List<MessageViewModel>();
+        return await GetMessagesAsync(chat.Id, lastMessageId);
     }
 }
