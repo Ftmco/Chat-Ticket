16e7cacf5e4ea7789bf634ded0a26da721188b56
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-06-01 09:22:20 -0700

    create comment services

---
+++ ChatTicket/ChTi.Service.Implemention/Comment/CommentGet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Service.Implemention;
+
+public class CommentGet : ICommentGet
+{
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ ChatTicket/ChTi.Service.Implemention/Comment/CommentViewModelService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChTi.Service.Implemention;
+
+public class CommentViewModelService : ICommentViewModel
+{
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
