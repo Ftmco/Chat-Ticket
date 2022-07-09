d8fa57c16c1f3b137a29234099c87ed5c629ffff
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-07-09 15:45:07 +0430

    Update PvController.cs

--- ChatTicket/ChTi.Web.Api/Controllers/Chat/PvController.cs
+++ ChatTicket/ChTi.Web.Api/Controllers/Chat/PvController.cs
@@ -33,4 +33,21 @@ public class PvController : ChatBaseController
 
     public async override Task<IActionResult> GetChatsAsync()
         => Ok(Success("گفتگو های خصوصی", "", await _pvGet.GetUserPvChatsAsync(Request.Headers)));
+
+    [HttpGet("Start")]
+    public async Task<IActionResult> PvChatAsync(Guid userId)
+    {
+        PvChatResponse pvChat = await _pvAction.StartPvChatAsync(Request.Headers, userId);
+        return pvChat.Status switch
+        {
+            ChatActionStatus.Success => Ok(Success("", "", pvChat.Chat)),
+            ChatActionStatus.UserNotAuthorized => Ok(Faild(403, "برای ایجاد گفتگو وارد حساب خود شوید", "")),
+            ChatActionStatus.Exception => Ok(ApiException()),
+            ChatActionStatus.ChatNotFound => Ok(Faild(404, "گفتگو یافت نشد", "")),
+            ChatActionStatus.AccessDenied => Ok(Faild(403, "برای ایجاد گفتگو وارد حساب خود شوید", "")),
+            ChatActionStatus.UserNotFound => Ok(Faild(404, "کاربر مورد نظر یافت نشد", "")),
+            ChatActionStatus.Illegal => Ok(Faild(409,"ایجاد گفتگو مجاز نیست","")),
+            _ => Ok(ApiException()),
+        };
+    }
 }
